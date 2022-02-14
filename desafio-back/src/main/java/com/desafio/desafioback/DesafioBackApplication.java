257d7c1b36fa017543e1e58843e0df1ffa2120b5
mat.azvd <mat.azvd@gmail.com>
2022-02-14 14:03:52 -0300

    inicializar repositório

---
+++ desafio-back/src/main/java/com/desafio/desafioback/DesafioBackApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackApplication.class, args);
+	}
+
+}
---
+++ desafio-back/src/test/java/com/desafio/desafioback/DesafioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
